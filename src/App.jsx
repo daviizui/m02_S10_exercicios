63176a4c41c97515cb92a8c1d07fbb1bc43e12ba
Davi Izui <davi.izui@gmail.com>
2025-08-11 14:22:05 -0300

    Implementando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import InstrumentosCard from "./pages/InstrumentosCard/InstrumentosCard";
+import InstrumentosTabela from "./pages/InstrumentosTabela/InstrumentosTabela";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" Component={InstrumentosCard} />
+        <Route path="/instrumentoTabela" Component={InstrumentosTabela} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
